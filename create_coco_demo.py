d710f2b5d69cb368c865600402a4a4a1e598fa1e
waspinator <patrickwasp@gmail.com>
2018-03-29 15:13:05 -0400

    initial

---
+++ create_coco_demo.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+from logzero import logger
+import datetime
+import json
+import os
+import re
+import fnmatch
+from PIL import Image
+import numpy as np
+from skimage import measure
+from pycocotools import mask
+from pycocoextra import pycocoextra
+
+IMAGE_DIR = 'example_data/images'
+ANNOTATION_DIR = 'example_data/annotations'
+
+INFO = {
+    "description": "ExampleDataset",
+    "url": "https://github.com/waspinator/pycocoextra",
+    "version": "0.1.0",
+    "year": 2018,
+    "contributor": "waspinator",
+    "date_created": datetime.datetime.utcnow().isoformat(' ')
+}
+
+LICENSES = [
+    {
+        "id": 1,
+        "name": "Attribution-NonCommercial-ShareAlike License",
+        "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+    }
+]
+
+CATEGORIES = [
+    {
+        'id': 1,
+        'name': 'blueberry',
+        'supercategory': 'fruit',
+    }
+]
+
+def main():
+
+    coco_output = {
+        "info": INFO,
+        "licenses": LICENSES,
+        "categories": CATEGORIES,
+        "images": [],
+        "annotations": []
+    }
+
+    image_id = 1
+    segmentation_id = 1
+    
+    for root, directories, files in os.walk(IMAGE_DIR):
+        file_types = ['*.jpeg', '*.jpg']
+        file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+        files = [os.path.join(root, f) for f in files]
+        files = [f for f in files if re.match(file_types, f)]
+
+        # go through each image
+        for i, filename in enumerate(files):
+            basename_no_extension = os.path.splitext(os.path.basename(filename))[0]
+            image = Image.open(filename)
+            image_info = pycocoextra.create_image_info(image_id, os.path.basename(filename), image.size[0], image.size[1])
+            coco_output["images"].append(image_info)
+
+            # go through each associated annotation
+            for root, directories, files in os.walk(ANNOTATION_DIR):
+                file_types = ['*.png']
+                file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+                file_name_prefix = basename_no_extension + '.*'
+                files = [os.path.join(root, f) for f in files]
+                files = [f for f in files if re.match(file_types, f)]
+                files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f))[0])]
+
+                for filename in files:
+                    basename_no_extension = os.path.splitext(os.path.basename(filename))[0]
+                    binary_mask = np.logical_not(np.array(Image.open(filename))).astype(np.uint8)
+                    category_info = {'id': 1, 'is_crowd': 'crowd' in basename_no_extension}
+                    annotation_info = pycocoextra.create_annotation_info(segmentation_id, image_id,
+                        category_info, binary_mask, image.size)
+                    coco_output["annotations"].append(annotation_info)
+
+                    segmentation_id = segmentation_id + 1
+
+            image_id = image_id + 1
+
+    with open('{}/coco_example.json'.format(ANNOTATION_DIR), 'w') as output_json_file:
+        json.dump(coco_output, output_json_file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pycocoextra/pycocoextra.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+import numpy as np
+from itertools import groupby
+from skimage import measure
+from PIL import Image
+from pycocotools import mask
+
+
+def resize_array(array, new_size):
+    image = Image.fromarray(array)
+    image = image.resize(new_size)
+    return np.asarray(image)
+
+def close_contour(contour):
+    if not np.array_equal(contour[0], contour[-1]):
+        contour = np.vstack((contour, contour[0]))
+    return contour
+
+def binary_mask_to_rle(binary_mask, invert=True):
+    rle = {'counts': [], 'size': list(binary_mask.shape)}
+    counts = rle.get('counts')
+    print(binary_mask)
+    for i, (value, elements) in enumerate(groupby(binary_mask.ravel(order='F'))):
+        if i == 0:
+            if value == 1 and not invert:
+                counts.append(0)
+            else:
+                counts.append(0)
+        counts.append(len(list(elements)))
+
+    return rle
+
+def binary_mask_to_polygon(binary_mask):
+    polygons = []
+    contours = measure.find_contours(binary_mask, 0.5)
+    for contour in contours:
+        contour = close_contour(contour)
+        contour = np.flip(contour, axis=1)
+        segmentation = contour.ravel().tolist()
+        polygons.append(segmentation)
+
+    return polygons
+
+def create_image_info(image_id, file_name, width, height, 
+                      date_captured=datetime.datetime.utcnow().isoformat(' '),
+                      license_id=1, coco_url="", flickr_url=""):
+
+    image_info = {
+            "id": image_id,
+            "file_name": file_name,
+            "width": width,
+            "height": height,
+            "date_captured": date_captured,
+            "license": license_id,
+            "coco_url": coco_url,
+            "flickr_url": flickr_url
+    }
+
+    return image_info
+
+def create_annotation_info(annotation_id, image_id, category_info, binary_mask, image_size):
+    binary_mask = resize_array(binary_mask, image_size)
+    binary_mask_encoded = mask.encode(np.asfortranarray(binary_mask.astype(np.uint8)))
+    bounding_box = mask.toBbox(binary_mask_encoded)
+    area = mask.area(binary_mask_encoded)
+
+    if category_info["is_crowd"]:
+        annotation_info = {
+            "id": annotation_id,
+            "image_id": image_id,
+            "category_id": category_info["id"],
+            "iscrowd": 1,
+            "area": area.tolist(),
+            "bbox": bounding_box.tolist(),
+            "segmentation": binary_mask_to_rle(binary_mask),
+            "width": binary_mask.shape[1],
+            "height": binary_mask.shape[0],
+        } 
+    else:
+        annotation_info = {
+            "id": annotation_id,
+            "image_id": image_id,
+            "category_id": category_info["id"],
+            "iscrowd": 0,
+            "area": area.tolist(),
+            "bbox": bounding_box.tolist(),
+            "segmentation": binary_mask_to_polygon(binary_mask),
+            "width": binary_mask.shape[1],
+            "height": binary_mask.shape[0]
+        }
+
+    return annotation_info
