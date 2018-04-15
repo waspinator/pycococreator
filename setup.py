48b452ce8f645fe81c4e1d77124a1dcd7849174e
waspinator <patrickwasp@gmail.com>
2018-04-14 22:32:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-# To install library to Python site-packages run "python setup.py  install"
+# To install library to Python site-packages run "python setup.py install"
 
 setup(name='pycococreatortools',
     packages=['pycococreatortools'],
@@ -11,7 +11,7 @@ setup(name='pycococreatortools',
     url = 'https://github.com/waspinator/pycococreator',
     author = 'waspinator',
     author_email = 'patrickwasp@gmail.com',
-    download_url = 'https://github.com/waspinator/pycococreator/archive/0.1.1.tar.gz',
+    download_url = 'https://github.com/waspinator/pycococreator/archive/0.1.4.tar.gz',
     keywords = ['coco', 'dataset', 'machine-learning'],
     install_requires=[
         'numpy', 'pillow', 'scikit-image'
