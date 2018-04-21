583d5d165be96ce0064a191696bf0c9a9245e407
waspinator <patrickwasp@gmail.com>
2018-04-21 01:26:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.extension import Extension
 setup(name='pycococreatortools',
     packages=['pycococreatortools'],
     package_dir = {'pycococreatortools': 'pycococreatortools'},
-    version='0.1.1',
+    version='0.1.5',
     description = 'Tools to create COCO datasets',
     url = 'https://github.com/waspinator/pycococreator',
     author = 'waspinator',
