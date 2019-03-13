da8f08f1c019855fea07b8173a956c90176b9b19
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2019-03-13 21:34:08 +0800

    Fix miss-leading example
    There is a potential bug in the given example, regarding the regular expression for file matching.

--- examples/shapes/shapes_to_coco.py
+++ examples/shapes/shapes_to_coco.py
@@ -60,7 +60,7 @@ def filter_for_annotations(root, files, image_filename):
     file_types = ['*.png']
     file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
     basename_no_extension = os.path.splitext(os.path.basename(image_filename))[0]
-    file_name_prefix = basename_no_extension + '.*'
+    file_name_prefix = basename_no_extension + '_.*'
     files = [os.path.join(root, f) for f in files]
     files = [f for f in files if re.match(file_types, f)]
     files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f))[0])]
